fed67afaa1010c6daba0533bb5e3b7edb58998bb
Maira Bello <maira.araujo@liferay.com>
2015-11-16 15:30:13 -0300

    Registers components on each class declaration/expression

--- index.js
+++ index.js
@@ -3,17 +3,37 @@
 module.exports = function(babel) {
   var t = babel.types;
 
+  function addComponentRegistrationCode(path, node, classId) {
+    // This check is necessary to prevent paths from being handled more than once
+    // which can happen because of actions from other plugins. For example, the
+    // plugin that handles ES2015 classes sometimes converts class declarations
+    // into class expressions, causing them to be readded to the tree and handled
+    // again.
+    if (node.visitedByMetalRegisterComponents || !node.superClass) {
+      return;
+    }
+    node.visitedByMetalRegisterComponents = true;
+
+    var fnId = t.identifier(classId.name + '.prototype.registerMetalComponent');
+    path.insertAfter(t.logicalExpression(
+      '&&',
+      fnId,
+      t.callExpression(fnId, [classId])
+    ));
+  }
+
   return {
     visitor: {
-      /**
-       * @param {!NodePath} nodePath
-       */
-      ClassDeclaration: function(nodePath) {
-        console.log('ClassDeclaration', nodePath);
+      ClassDeclaration: function(path) {
+        path.insertAfter(addComponentRegistrationCode(path, path.node, path.node.id));
       },
 
-      ClassExpression: function(nodePath) {
-        console.log('ClassExpression', nodePath);
+      VariableDeclaration: function(path) {
+        path.node.declarations.forEach(function(declaration) {
+          if (t.isClassExpression(declaration.init)) {
+            addComponentRegistrationCode(path, declaration.init, declaration.id);
+          }
+        });
       }
     }
   };
--- test/test.js
+++ test/test.js
@@ -3,12 +3,82 @@
 var assert = require('assert');
 var babel = require('babel-core');
 var plugin = require('../index');
+var sinon = require('sinon');
+
+global.myGlobals = {};
+var babelOptions = {
+  filename: 'foo/Foo.js',
+  plugins: [plugin, ['globals', {globalName: 'myGlobals'}]],
+  presets: ['es2015']
+};
 
 module.exports = {
+  setUp: function(done) {
+    global.Component = sinon.stub();
+    global.Component.prototype.registerMetalComponent = sinon.stub();
+    done();
+  },
+
   testPluginExistence: function(test) {
     assert.doesNotThrow(function() {
       babel.transform('var a = 2;', {plugins: [plugin]});
     });
+    test.done();
+  },
+
+  testClassDeclarationNoSuperClass: function(test) {
+    var code = 'class Foo {}\nexport default Foo;\nexport default Foo;';
+    var result = babel.transform(code, babelOptions);
+    eval.call(global, result.code);
+    assert.strictEqual(-1, result.code.indexOf('Foo.prototype.registerMetalComponent'));
+    assert.strictEqual(0, global.Component.prototype.registerMetalComponent.callCount);
+    test.done();
+  },
+
+  testClassDeclarationNoRegistrationFn: function(test) {
+    var code = 'class Bar {}\nclass Foo extends Bar {}\nexport default Foo;';
+    var result = babel.transform(code, babelOptions);
+    eval.call(global, result.code);
+    assert.notStrictEqual(-1, result.code.indexOf('Foo.prototype.registerMetalComponent'));
+    assert.strictEqual(0, global.Component.prototype.registerMetalComponent.callCount);
+    test.done();
+  },
+
+  testClassDeclarationWithRegistrationFn: function(test) {
+    var code = 'class Foo extends Component {}\nexport default Foo;';
+    var result = babel.transform(code, babelOptions);
+    eval.call(global, result.code);
+    assert.notStrictEqual(-1, result.code.indexOf('Foo.prototype.registerMetalComponent'));
+    assert.strictEqual(1, global.myGlobals.Foo.prototype.registerMetalComponent.callCount);
+    assert.strictEqual(global.myGlobals.Foo, global.myGlobals.Foo.prototype.registerMetalComponent.args[0][0]);
+    test.done();
+  },
+
+  testClassExpressionNoSuperClass: function(test) {
+    var code = 'var MyFoo = class Foo {}\nexport default MyFoo;';
+    var result = babel.transform(code, babelOptions);
+    eval.call(global, result.code);
+    assert.strictEqual(-1, result.code.indexOf('MyFoo.prototype.registerMetalComponent'));
+    assert.strictEqual(0, global.Component.prototype.registerMetalComponent.callCount);
+    test.done();
+  },
+
+  testClassExpressionNoRegistrationFn: function(test) {
+    var code = 'class Bar {}\nvar MyFoo = class Foo extends Bar {}\nexport default MyFoo;';
+    var result = babel.transform(code, babelOptions);
+    eval.call(global, result.code);
+    assert.notStrictEqual(-1, result.code.indexOf('MyFoo.prototype.registerMetalComponent'));
+    assert.strictEqual(0, global.Component.prototype.registerMetalComponent.callCount);
+    test.done();
+  },
+
+  testClassExpressionWithRegistrationFn: function(test) {
+    var code = 'var MyFoo = class Foo extends Component {}\nexport default MyFoo;';
+    var result = babel.transform(code, babelOptions);
+    eval.call(global, result.code);
+    assert.notStrictEqual(-1, result.code.indexOf('MyFoo.prototype.registerMetalComponent'));
+    assert.strictEqual(1, global.myGlobals.Foo.prototype.registerMetalComponent.callCount);
+    assert.strictEqual(global.myGlobals.Foo, global.myGlobals.Foo.prototype.registerMetalComponent.args[0][0]);
     test.done();
   }
 };
